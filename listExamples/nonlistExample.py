7e98f5a3f08cdaf97a7542d6d42a3ec9db9027f3
Satoru Hayasaka <hayasaka@utexas.edu>
2016-08-23 16:29:58 -0500

    initial commit

---
+++ listExamples/nonlistExample.py
@@ -0,0 +1,10 @@
+memberName1 = 'Paul'
+memberName2 = 'John'
+memberName3 = 'George'
+memberName4 = 'Ringo'
+
+print('The band members are:')
+print('   ' + memberName1)
+print('   ' + memberName2)
+print('   ' + memberName3)
+print('   ' + memberName4)
