45b8db3dc2449cbd4617aa8da94f859b90f45f95
Satoru Hayasaka <hayasaka@utexas.edu>
2016-09-06 13:45:28 -0500

    Celsius to Fahrenheit conversion

---
+++ Homework/Homework1/C2F.py
@@ -0,0 +1,6 @@
+# input of temperature in Fahrenheit
+tempC = int(input('What is the temperature [C]? '))
+# the formula for conversion
+tempF = tempC * 9/5 + 32
+# the output
+print('The temperature is ' + str(tempF) + ' Fahrenheit.')
