aeb635294d11f4a28223bc46bf6219fa6bc8a3ae
Satoru Hayasaka <hayasaka@utexas.edu>
2016-09-06 13:45:05 -0500

    How many apples

---
+++ Homework/Homework1/Apples.py
@@ -0,0 +1,4 @@
+wallet = float(input('How much money do you have?'))
+apple = float(input('How much is an apple?'))
+numApple = wallet // apple
+print('You can buy ' + str(int(numApple)) + ' apples')
