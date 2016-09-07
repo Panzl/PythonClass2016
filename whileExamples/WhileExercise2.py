64600b583ac50313af6c20e59560d7de4a628adf
Satoru Hayasaka <hayasaka@utexas.edu>
2016-09-07 14:15:25 -0500

    delete

--- whileExamples/WhileExercise2.py
+++
@@ -1,8 +0,0 @@
-battery = 16
-while True:
-    if battery >= 100:
-        break
-    battery = battery + 3
-    print('The battery is ' + str(battery) + '% charged')
-
-print('The battery is fully charged!')
