9d235b470d9ce9cd5e26250f4173f476dedf2c4e
Satoru Hayasaka <hayasaka@utexas.edu>
2016-09-07 14:11:47 -0500

    deleted

--- whileExamples/WhileExample3.py
+++
@@ -1,7 +0,0 @@
-currentBalance = 4000
-payment = 250
-while currentBalance > 0:
-    print('Current balance is $' + str(currentBalance))
-    currentBalance = currentBalance - payment
-
-print('Congrats! You paid off the balance!')
