e8f1250605030d69a45138b2359ed908b794b342
Satoru Hayasaka <hayasaka@utexas.edu>
2016-09-08 13:15:39 -0500

    Guessing a word

---
+++ whileExamples/GuessWord.py
@@ -0,0 +1,5 @@
+word = input('Please enter the password. ')                  
+while word != 'keyboard':
+    print('Did not match the secret password...')
+    word = input('Please enter the password. ')                  
+print('You have entered the correct password!!')                 
