7ef166de130e4c7b8f0cc70e7060236b42d924a0
Satoru Hayasaka <hayasaka@utexas.edu>
2016-09-08 13:13:49 -0500

    Charging a phone

---
+++ whileExamples/ChargePhone.py
@@ -0,0 +1,8 @@
+charge = 16    #initial charge
+
+while charge<100:
+    print('Your phone is ' + str(charge) + '% charged')
+    charge += 3   # the phone is charged 3% during this period
+
+# message that the phone is fully charged
+print('Fully charged!')
