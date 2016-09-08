d67d937ecc53c36786fe4144f91a5ec63142fb4c
Satoru Hayasaka <hayasaka@utexas.edu>
2016-09-08 13:17:05 -0500

    Guessing a word with a break statement

---
+++ whileExamples/GuessWordWithBreak.py
@@ -0,0 +1,7 @@
+while True:
+    word = input('Please enter the password. ')
+    if word == 'laptop':
+        break
+    print('Incorrect password!')
+    
+print('You have entered the correct password!!')                 
