d8ebcfdffb08dac0157c4fae30236afbe25c850f
Satoru Hayasaka <hayasaka@utexas.edu>
2016-09-13 12:15:36 -0500

    Allowance exercise

--- forExamples/Allowance.py
+++ forExamples/Allowance.py
@@ -1,4 +1,5 @@
 allowance = 1
+print('Your allowance for this month is $' + str(allowance))
 for i in range(24):
     allowance = allowance * 2
     print('Your allowance for this month is $' + str(allowance))
