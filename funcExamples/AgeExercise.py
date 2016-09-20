607490d2254598af63f3ee4a9691ea098e859b73
Satoru Hayasaka <hayasaka@utexas.edu>
2016-09-20 13:40:32 -0500

    Exercise question asking age

---
+++ funcExamples/AgeExercise.py
@@ -0,0 +1,10 @@
+def PrintAge(age):
+    print('You are ' + str(age) + ' years old')
+
+def AgeStudy(age):
+    PrintAge(age)
+    if age<=30:
+        print('You can participate in the study')
+    else:
+        print('You are ineliglble to participate in the study')
+
