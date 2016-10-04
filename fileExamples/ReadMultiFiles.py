f149c27c5b55645204b79df4c84443f32bf84ce3
Satoru Hayasaka <hayasaka@utexas.edu>
2016-10-04 09:42:27 -0500

    Reading multiple files

---
+++ fileExamples/ReadMultiFiles.py
@@ -0,0 +1,51 @@
+import os
+
+def readfile(fName):
+    # open file
+    infile = open(fName,'r')
+    # initialize variables
+    trial = []
+    onsetTime = []
+    respTime = []
+    # read the first line
+    line = infile.readline()
+    # keep reading files until the last line
+    while line:
+        inString = line.strip().split()
+        inNumber = [int(i) for i in inString]
+        trial.append(inNumber[0])
+        onsetTime.append(inNumber[1])
+        respTime.append(inNumber[2])
+        # read the next line
+        line = infile.readline()
+    return trial, onsetTime, respTime
+
+
+# changing the working directory to the appropriate directory
+WorkDir = 'TestDir'  #this needs to be customized
+os.chdir(WorkDir)
+
+# checking the content of the directory
+listFiles = os.listdir()
+
+# initializing the variables
+subjID = []
+trialNum = []
+onset = []
+RT = []
+
+# for loop to read files and store data
+for iFile in listFiles:
+    iTrial, iOnset, iRT = readfile(iFile)
+    iSubj = [iFile.replace('sub','').replace('.txt','')] * len(iTrial)
+    subjID += iSubj
+    trialNum+= iTrial
+    onset += iOnset
+    RT += iRT
+
+
+# printing out what was read
+print('ID\tTrial\tOnset\tRT')
+for i in range(len(subjID)):
+    print(subjID[i],trialNum[i],onset[i],RT[i],sep='\t')
+
