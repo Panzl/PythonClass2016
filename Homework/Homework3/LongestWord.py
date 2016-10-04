a8541d48743b9a483fd5843d543d634580f213d0
Satoru Hayasaka <hayasaka@utexas.edu>
2016-10-04 14:41:37 -0500

    Hidden word

---
+++ Homework/Homework3/LongestWord.py
@@ -0,0 +1,13 @@
+def longword(inList):
+    maxLen = 0
+    maxWord = ''
+    for iWord in inList:
+        if len(iWord)>maxLen:
+            maxLen = len(iWord)
+            maxWord = iWord
+    for i in range(-1,-maxLen-1,-1):
+        print(maxWord[i], end='')
+    print()
+    return maxLen
+
+words = ['zucchini', 'chess', 'pie', 'engine', 'tire']
