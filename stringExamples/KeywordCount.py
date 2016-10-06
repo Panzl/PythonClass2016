f516646c2d029e592a77ddd3611c30c87f34afb5
Satoru Hayasaka <hayasaka@utexas.edu>
2016-10-06 13:49:16 -0500

    Keyword count exercise

---
+++ stringExamples/KeywordCount.py
@@ -0,0 +1,38 @@
+abstract = '''
+The friendship paradox is the phenomenon that in social networks,
+people on average have fewer friends than their friends do. The
+generalized friendship paradox is an extension to attributes other
+than the number of friends. The friendship paradox and its generalized
+version have gathered recent attention due to the information they
+provide about network structure and local inequalities. In this paper,
+we propose several measures of nodal qualities which capture different
+aspects of their activities and influence in online social networks.
+Using these measures we analyse the prevalence of the generalized
+friendship paradox over Twitter and we report high levels of prevalence
+(up to over 90% of nodes). We contend that this prevalence of the
+friendship paradox and its generalized version arise because of the
+hierarchical nature of the connections in the network. This hierarchy
+is nested as opposed to being star-like. We conclude that these
+paradoxes are collective phenomena not created merely by a minority
+of well-connected or high-attribute nodes. Moreover, our results
+show that a large fraction of individuals can experience the
+generalized friendship paradox even in the absence of a significant
+correlation between degrees and attributes.
+'''
+
+def depunct(inString):
+    outString = inString.replace(',','')
+    outString = outString.replace('.','')
+    outString = outString.replace('(','')
+    outString = outString.replace(')','')
+    return outString
+
+def KeywordCount(inString, keyword):
+    inWords = inString.lower().split()
+    count = 0
+    for iWord in inWords:
+        if iWord == keyword:
+            count += 1
+    return count
+
+
